96e113e3f218ded32f778023cbb9765eadb4dde2
NikVolf <nikvolf@gmail.com>
2019-01-09 13:53:17 +0300

    anchors -> anchor

--- storage/src/anchor.rs
+++ storage/src/anchor.rs
@@ -14,4 +14,3 @@ pub trait AnchorProvider {
 			)
 	}
 }
-
--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -25,7 +25,7 @@ mod transaction_meta;
 mod transaction_provider;
 mod nullifier;
 mod tree_state;
-mod anchors;
+mod anchor;
 
 pub use primitives::{hash, bytes};
 
@@ -42,7 +42,7 @@ pub use transaction_meta::TransactionMeta;
 pub use transaction_provider::{TransactionProvider, TransactionOutputProvider, TransactionMetaProvider};
 pub use nullifier::{Nullifier, NullifierTracker};
 pub use tree_state::{TreeState, H32 as H32TreeDim, Dim as TreeDim, RegularTreeState};
-pub use anchors::AnchorProvider;
+pub use anchor::AnchorProvider;
 
 /// Epoch tag.
 ///
