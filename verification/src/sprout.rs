df5e313352b9c4fcbab587fc31d8fcd835551a35
NikVolf <nikvolf@gmail.com>
2019-03-11 17:45:00 +0300

    add more test vectors

--- verification/src/sprout.rs
+++ verification/src/sprout.rs
@@ -46,6 +46,55 @@ mod tests {
 			),
 			hash("a8cba69f1fa329c055756b4af900f8a00b61e44f4cb8a1824ceb58b90a5b8113"),
 		);
+
+		assert_eq!(
+			compute_hsig(
+				hash("0000000000000000000000000000000000000000000000000000000000000000"),
+				[
+					hash("0000000000000000000000000000000000000000000000000000000000000000"),
+					hash("0000000000000000000000000000000000000000000000000000000000000000"),
+				],
+				hash("0000000000000000000000000000000000000000000000000000000000000000"),
+			),
+			hash("697322276b5dd93b12fb1fcbd2144b2960f24c73aac6c6a0811447be1e7f1e19"),
+		);
+
+		assert_eq!(
+			compute_hsig(
+				hash("1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"),
+				[
+					hash("1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"),
+					hash("1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"),
+				],
+				hash("1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"),
+			),
+			hash("b61110ec162693bc3d9ca7fb0eec3afd2e278e2f41394b3ff11d7cb761ad4b27"),
+		);
+
+		assert_eq!(
+			compute_hsig(
+				hash("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+				[
+					hash("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+					hash("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+				],
+				hash("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+			),
+			hash("4961048919f0ca79d49c9378c36a91a8767060001f4212fe6f7d426f3ccf9f32"),
+		);
+
+
+		assert_eq!(
+			compute_hsig(
+				hash("1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"),
+				[
+					hash("1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"),
+					hash("1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"),
+				],
+				hash("1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"),
+			),
+			hash("b61110ec162693bc3d9ca7fb0eec3afd2e278e2f41394b3ff11d7cb761ad4b27"),
+		);
 	}
 
 }
