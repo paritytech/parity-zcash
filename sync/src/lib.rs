5d265828302f8065238cbfc009b0d58f91148f31
Svyatoslav Nikolsky <svyatonik@gmail.com>
2019-04-05 11:27:19 +0300

    name all (excl main) threads

--- sync/src/lib.rs
+++ sync/src/lib.rs
@@ -117,8 +117,22 @@ pub fn create_local_sync_node(consensus: ConsensusParams, db: storage::SharedSto
 	let sync_server = Arc::new(ServerImpl::new(peers.clone(), db.clone(), memory_pool.clone(), sync_executor.clone()));
 	let sync_client_core = SynchronizationClientCore::new(sync_client_config, sync_state.clone(), peers.clone(), sync_executor.clone(), sync_chain);
 	let verifier_sink = Arc::new(CoreVerificationSink::new(sync_client_core.clone()));
-	let light_verifier = AsyncVerifier::new(light_chain_verifier, db.clone(), memory_pool.clone(), verifier_sink.clone(), verification_params.clone());
-	let heavy_verifier = AsyncVerifier::new(heavy_chain_verifier, db.clone(), memory_pool.clone(), verifier_sink, verification_params);
+	let light_verifier = AsyncVerifier::new(
+		"Light verification".into(),
+		light_chain_verifier,
+		db.clone(),
+		memory_pool.clone(),
+		verifier_sink.clone(),
+		verification_params.clone(),
+	);
+	let heavy_verifier = AsyncVerifier::new(
+		"Heavy verification".into(),
+		heavy_chain_verifier,
+		db.clone(),
+		memory_pool.clone(),
+		verifier_sink,
+		verification_params,
+	);
 	let sync_client = SynchronizationClient::new(sync_state.clone(), sync_client_core, light_verifier, heavy_verifier);
 	Arc::new(SyncNode::new(consensus, db, memory_pool, peers, sync_state, sync_client, sync_server))
 }
